741f84a07627efa90f5e1253b41d015e4d77d12d
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-11 14:34:23 +0300

    Add Favico

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Nav from './components/Nav';
+import Form from './components/Form';
 
 function App() {
   return (
-    <h1>Hurray!!!</h1>
+    <div className="ui container-fluid">
+      <Nav />
+      <Form />
+    </div>
   );
 }
 
