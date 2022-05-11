52039714bae07b6fddf4e663b72f0ed64d53bc33
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-11 17:30:02 +0300

    Installed Axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React from 'react'
 import './App.css';
 import Nav from './components/Nav';
 import Form from './components/Form';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="ui container-fluid">
-      <Nav />
-      <Form />
-    </div>
-  );
+class App extends React.Component {
+
+  onSearchSubmit(type) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: type },
+      header: {
+        Authorization: 'Client-ID zNCIK0Lrm2VATnf_vJCb93wXV7HxL2_LsBEJTAWlj8s'
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="ui container-fluid">
+        <Nav />
+        <Form onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,31 @@
 import React from 'react'
 
-function Form() {
-  return (
-      <div className='ui container'>
-          <div className="ui segment">
-              <div className="ui form">
-                  <div className="field">
-                      <label>Image Search</label>
-                      <input type="text" />
+class Form extends React.Component {
+    state = { type: '' }
+    
+    onFormSubmit(e) {
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className='ui container'>
+                <div className="ui segment">
+                    <div className="ui form" onsSubmit={this.onFormSubmit}>
+                        <div className="field">
+                            <label>Image Search</label>
+                            <input
+                                type="text"
+                                value={this.state.type}
+                                onChange={(e) => this.setValue(e.target.value)}
+                            />
+                            <p>You've typed!!</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
-          </div> 
-    </div>
-  )
+            </div>
+        )
+    }
 }
 
 export default Form
