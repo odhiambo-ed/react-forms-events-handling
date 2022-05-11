a0433946b0e86c331773fa51012b647aa6200e68
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-11 14:33:52 +0300

    Add Form

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Form() {
+  return (
+      <div className='ui container'>
+          <div className="ui segment">
+              <div className="ui form">
+                  <div className="field">
+                      <label>Image Search</label>
+                      <input type="text" />
+                </div>
+              </div>
+          </div> 
+    </div>
+  )
+}
+
+export default Form
