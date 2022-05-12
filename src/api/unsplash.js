b2ef687803b1840a4c48fe721eafc7b7181b3ef3
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-13 01:24:28 +0300

    Add Unsplash API

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,8 @@
-import React from 'react'
 import axios from 'axios'
 
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: ''
-    }
+        Authorization: 'Client-ID zNCIK0Lrm2VATnf_vJCb93wXV7HxL2_LsBEJTAWlj8s',
+    },
 })
